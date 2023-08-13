e6d775ae8e1735bfdded63ebbf28d0a0097966ee
Programmer Bikash <bikash19081998dey@gmail.com>
2023-08-13 10:07:40 +0600

    [DONE] eslint setup

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
 
-// parser
+//parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working successfully')
+// Application routes
+app.use('/api/v1/users/', usersRouter)
+
+//Testing
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working Successfully')
 })
 
 export default app
